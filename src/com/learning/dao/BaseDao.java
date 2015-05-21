dc19deb6f6923929d001283069dc621e26b60592
pengsheng <pengsheng@youngfriend.cn>
2015-05-21 19:17:01 +0800

    add html

---
+++ src/com/learning/dao/BaseDao.java
@@ -0,0 +1,7 @@
+package com.learning.dao;
+
+/**
+ * Created by pengsheng on 15-05-21.
+ */
+public interface BaseDao {
+}
---
+++ src/com/learning/dao/impl/BaseDaoImpl.java
@@ -0,0 +1,13 @@
+package com.learning.dao.impl;
+
+import com.learning.dao.BaseDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by pengsheng on 15-05-21.
+ */
+@Repository
+public class BaseDaoImpl implements BaseDao {
+
+
+}
